58080b137a5f473a2de778a10f2ad1da00bfa479
Artur Jové Fernández <72749340+redsoldier1996@users.noreply.github.com>
2021-10-01 09:54:21 +0200

    HTML basic skeleton. Data structure.

---
+++ src/app/plate-options.ts
@@ -0,0 +1,11 @@
+export interface Entree{
+    id: number;
+    plate: string;
+    price: number;
+}
+
+export interface Side{
+    id: number;
+    plate: string;
+    price: number;
+}
---
+++ src/app/plates.ts
@@ -0,0 +1,17 @@
+import { Entree, Side } from "./plate-options";
+
+export const Entrees: Entree[] = [
+    { id: 1, plate: 'firstEntree', price: 8 },
+    { id: 2, plate: 'secondEntree', price: 10 },
+    { id: 3, plate: 'thirdEntree', price: 14 },
+    { id: 4, plate: 'fourthEntree', price: 7 },
+    { id: 5, plate: 'fifthEntree', price: 9 }
+]
+
+export const Sides: Side[] = [
+    { id: 1, plate: 'firstSide', price: 15 },
+    { id: 2, plate: 'secondSide', price: 14 },
+    { id: 3, plate: 'thirdSide', price: 16 },
+    { id: 4, plate: 'fourthSide', price: 10 },
+    { id: 5, plate: 'fifthSide', price: 11 },
+]
